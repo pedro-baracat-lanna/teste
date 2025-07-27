d0d18c72567b59eb8bddaa7eeb1c3586fed17a22
pedro-baracat-lanna <pedro.baracat.lanna@gmail.com>
2025-07-27 13:30:59 -0300

    Primeiro commit

---
+++ teste.py
@@ -0,0 +1,14 @@
+# Prompt user for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+total = num1 + num2
+
+# Display the result
+print('result:',total)
+
+
+
+
+
